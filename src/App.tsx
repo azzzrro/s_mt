cec2734c6db0cdc82569cb6cf7f0d3661f20a4eb
azzzrro <helloazzzrro@gmail.com>
2023-12-30 15:12:33 +0530

    initial-commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import LoginPage from "./Pages/LoginPage"
+
+function App() {
+
+  return (
+    <>
+      <LoginPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import Login from "../Components/Login"
+
+const LoginPage = () => {
+  return (
+    <>
+    <Login/>    
+    </>
+  )
+}
+
+export default LoginPage
